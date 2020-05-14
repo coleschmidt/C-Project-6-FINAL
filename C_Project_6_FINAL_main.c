08222869683e61fb7e2795ae329fd6ccfc57f622
coleschmidt <colecschmidt@gmail.com>
2020-05-14 15:29:48 -0500

    new test to see if scanf works for range

--- C_Project_6_FINAL_main.c
+++ C_Project_6_FINAL_main.c
@@ -43,10 +43,10 @@ int main(int argc, char *argv[]) {
 	//as for the requirement of the project, the checked numbers must be within a range
 	//this is an algorithm that asks the user what the numbers they want the range to be within
 	//as for an ongoing execution bug that stalls the program.
-	/*printf("What two numbers do you want for the lower and upper limit?\n");
+	printf("What two numbers do you want for the lower and upper limit?\n");
 	printf("It can be any number as long it's an interger between 1 to 350\n");
 	scanf("%d\n", &lower_limit);
-	scanf("%d\n" , &upper_limit);*/
+	scanf("%d\n" , &upper_limit);
 
 	//as for another project reuqirement the range must be within 1 to 350.
 	//this algorithim is supposed to ask the user to resummit the numbers if it doesn't fall under 1 to 350. 
